2212db58a84c71a1abebf147166cbf494a745d2f
bhaskernitt <bhasker.nandkishor@gmail.com>
2021-01-20 17:30:03 +0530

    Update chargbee-cli.rb

--- Formula/chargbee-cli.rb
+++ Formula/chargbee-cli.rb
@@ -3,12 +3,13 @@
 require "language/python3"
 
 class ChargbeeCli < Formula
+  include Language::Python::Virtualenv
   desc "unleash the chargebee-cli"
   homepage "https://github.com/bhaskerstreet/chargebee-cli"
   url "http://github.com/bhaskernitt/homebrew-brew/blob/main/chargbee-cli.rb"
   version "2.16.0"
   sha256 "e5b28ba7e29492fc4d38dbc0399cf627cb5492e26fbefe5b374d681dssdd5b2eb"
-  depends_on "python3"
+  depends_on "python@3.9"
 
   def install
     libexec.install Dir["*"]
