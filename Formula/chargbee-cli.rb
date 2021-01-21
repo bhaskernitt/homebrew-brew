558f8542a88da5603a970c1bf36613fdfed940d8
bhaskernitt <bhasker.nandkishor@gmail.com>
2021-01-21 09:59:27 +0530

    Update chargbee-cli.rb

--- Formula/chargbee-cli.rb
+++ Formula/chargbee-cli.rb
@@ -2,8 +2,8 @@ class ChargbeeCli < Formula
   include Language::Python::Virtualenv
   desc "unleash the chargebee-cli"
   homepage "https://github.com/bhaskerstreet/chargebee-cli"
-  url "http://github.com/bhaskernitt/homebrew-brew/blob/main/chargbee-cli.rb"
-  version "2.16.0"
+  url "https://github.com/bhaskernitt/chargebee-cli/blob/978c84fd6e636f5f9341ccb04d8c70a011423065/bin/chargebee-cli-0.0.18.tar.gz?raw=true"
+  version "0.0.18"
   sha256 "e5b28ba7e29492fc4d38dbc0399cf627cb5492e26fbefe5b374d681dssdd5b2eb"
   depends_on "python@3.9"
 
