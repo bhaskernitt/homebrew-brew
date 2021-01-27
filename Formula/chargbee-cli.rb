b17ac4d811e41fc23e1cd915cd26dd8ba4e53ef9
bhaskernitt <bhasker.nandkishor@gmail.com>
2021-01-27 10:12:28 +0530

    Update chargbee-cli.rb

--- Formula/chargbee-cli.rb
+++ Formula/chargbee-cli.rb
@@ -2,7 +2,7 @@ class ChargbeeCli < Formula
   include Language::Python::Virtualenv
   desc "unleash the chargebee-cli"
   homepage "https://github.com/bhaskerstreet/chargebee-cli"
-  url "https://github.com/bhaskernitt/chargebee-cli/raw/main/bin/chargebee-cli-0.0.19.tar.gz"
+  url "https://github.com/bhaskernitt/chargebee-cli/archive/chargebee-cli-0.0.19-release.tar.gz"
   version "0.0.19"
   sha256 "44fdd18af2f1183f9ed0fe3011086598be7d3f33230d95bc79c9514ac4ea2ab5"
   depends_on "python@3.9"
