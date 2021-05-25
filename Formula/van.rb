ff4b4b48480aecc40965e54eac6ddbdbda5bd48c
bhaskernitt <bhasker.nandkishor@gmail.com>
2021-05-25 10:28:16 +0530

    Update van.rb

--- Formula/van.rb
+++ Formula/van.rb
@@ -2,7 +2,7 @@ class Van < Formula
   include Language::Python::Virtualenv
   desc "book slot for ur vaccination !!"
   homepage "https://github.com/bhaskerstreet/vaccine-availability-notifier"
-  url "https://github.com/bhaskernitt/vaccine-availability-notifier/archive/0.0.15.tar.gz"
+  url "https://github.com/bhaskernitt/vaccine-availability-notifier/archive/0.0.16.tar.gz"
   version "0.0.16"
   sha256 "24a3dc76034a0c09bf1f896a65b6d8bf837167826c2d65a89c71668426836bb3"
   depends_on "python@3.9"
